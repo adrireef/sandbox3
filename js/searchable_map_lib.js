b35a059453f54b1c406d3d48aa9c46c81daafdc0
Annalisa Minelli <annalisapg@users.noreply.github.com>
2020-08-27 17:09:24 +0200

    Update searchable_map_lib.js

--- js/searchable_map_lib.js
+++ js/searchable_map_lib.js
@@ -77,7 +77,7 @@ var SearchableMapLib = {
 	  
 	  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(SearchableMapLib.map);
 
-//	SearchableMapLib.OSMGeocoder = new L.Control.OSMGeocoder({placeholder: 'Search location...'});
+	SearchableMapLib.OSMGeocoder = new L.Control.OSMGeocoder({placeholder: 'Search location...'});
 //	SearchableMapLib.map.addControl(SearchableMapLib.OSMGeocoder);
 
       //add hover info control
@@ -176,7 +176,7 @@ var SearchableMapLib = {
 //      SearchableMapLib.geocoder.geocode( { 'address': address }, function(results, status) {
 //      SearchableMapLib.currentPinpoint = [results[0].geometry.location.lat(), results[0].geometry.location.lng()];
 	
-	SearchableMapLib.OSMGeocoder({ 'address': address }, function(results) {
+	SearchableMapLib.OSMGeocoder.createSearchResults({ 'address': address }, function(results) {
 	  SearchableMapLib.currentPinpoint = [results[0].lat(), results[0].lon()];
       $.address.parameter('address', encodeURIComponent(address));
       $.address.parameter('radius', SearchableMapLib.radius);
